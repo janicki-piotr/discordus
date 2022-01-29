b9b05aa0368bdf87b25ec902f464fed02cc16414
Piotr Janicki <pcpiotr@gmail.com>
2022-01-29 17:00:06 +0100

    Discord send fix

--- src/main/kotlin/pl/redny/listener/moderator/video/YoutubeListener.kt
+++ src/main/kotlin/pl/redny/listener/moderator/video/YoutubeListener.kt
@@ -4,8 +4,10 @@ import club.minnced.jda.reactor.on
 import kotlinx.coroutines.reactor.mono
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import org.eclipse.microprofile.config.inject.ConfigProperty
+import pl.redny.listener.DiscordException
 import pl.redny.listener.EventListener
 import pl.redny.service.video.VideoEncoder
 import pl.redny.service.video.YoutubeService
@@ -16,6 +18,7 @@ import javax.enterprise.context.ApplicationScoped
 @ApplicationScoped
 class YoutubeListener(
     @ConfigProperty(name = "app.video.cache.path", defaultValue = "/videos") val destination: String,
+    @ConfigProperty(name = "max-attachment-size", defaultValue = "8388608") val maxAttachmentSize: Long,
     val jda: JDA,
     val youtubeService: YoutubeService,
     val videoEncoder: VideoEncoder
@@ -28,10 +31,18 @@ class YoutubeListener(
     private fun handleCommand(it: MessageReceivedEvent) = mono {
         val channel = it.channel
         handleDownloading(it.message.contentRaw).onSuccess {
-            handleEncoding(it).onSuccess {
-                channel.sendMessage("Reupload").addFile(it).queue()
-            }
+            handleEncoding(it).onSuccess { uploadFile("Reupload", it, channel) }
         }.onFailure { channel.sendMessage("Error processing file").queue() }
+    }
+
+    private fun uploadFile(message: String, file: File, channel: MessageChannel): Result<Unit> {
+        if (message.isEmpty()) {
+            return Result.failure(DiscordException("Message cannot be empty."))
+        }
+        if (file.length() > maxAttachmentSize) {
+            return Result.failure(DiscordException("Attachment exceeded max size "))
+        }
+        return Result.runCatching { channel.sendMessage(message).addFile(file).queue() }
     }
 
     private fun handleDownloading(url: String): Result<File> = youtubeService.download(url, destination)
